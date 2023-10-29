58e49b582fd0364c4d43dcd540b3aa278bcffeca
Matthew Cavallo <matthewcavallo@Matthews-Air.attlocal.net>
2023-10-29 15:07:05 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DependencyInjectionExample
+{
+    public interface IService
+    {
+        void Serve();
+    }
+
+    public class Service : IService
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Service Called");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create service collection
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+
+            // Create service provider
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            // Get service from DI and use it
+            IService service = serviceProvider.GetService<IService>();
+            service.Serve();
+
+            Console.ReadLine();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<IService, Service>();
+        }
+    }
+}
